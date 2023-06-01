33267fe34d097212cbeab7cd3bb9ca3c5771e784
kmanka1 <77521313+kmanka1@users.noreply.github.com>
2023-06-01 15:33:25 -0400

    Added dropdown menu to 360 dashboard table

--- frontend/src/components/data-tables/360-data-table/columns.tsx
+++ frontend/src/components/data-tables/360-data-table/columns.tsx
@@ -1,6 +1,16 @@
 "use client"
 
 import { ColumnDef } from "@tanstack/react-table"
+import { Button } from "@/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { MoreHorizontal } from "lucide-react"
 
 export type Upload = {
     id: string
@@ -42,6 +52,35 @@ export const columns: ColumnDef<Upload>[] = [
     {
       accessorKey: "date",
       header: "Date created",
+    },
+
+    {
+      id: "actions",
+      cell: ({ row }) => {
+        const payment = row.original
+   
+        return (
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="ghost" className="h-8 w-8 p-0">
+                <span className="sr-only">Open menu</span>
+                <MoreHorizontal className="h-4 w-4" />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuLabel>Actions</DropdownMenuLabel>
+              <DropdownMenuItem
+                onClick={() => navigator.clipboard.writeText(payment.id)}
+              >
+                Copy link
+              </DropdownMenuItem>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>Rename</DropdownMenuItem>
+              <DropdownMenuItem>Delete</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        )
+      },
     },
   ]
 
