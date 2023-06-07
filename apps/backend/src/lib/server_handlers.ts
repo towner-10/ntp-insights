61d805f9e0ac26cf2e297f4fb83d0cefe5c48ead
Collin Town <collintown@gmail.com>
2023-06-07 11:33:11 -0400

    Updated database build

---
+++ apps/backend/src/lib/server_handlers.ts
@@ -0,0 +1,178 @@
+import http from 'http';
+import formidable from 'formidable';
+import parseCookies from '../utils/parseCookies';
+import { promises as fs } from 'fs';
+import { logger } from '../utils/logger';
+import { ImageResult } from 'types';
+import { verifyAccessToken } from '../db';
+
+const IMAGE_DIRECTORY = './images';
+
+export const handleRequest = async (
+	req: http.IncomingMessage,
+	res: http.ServerResponse<http.IncomingMessage> & {
+		req: http.IncomingMessage;
+	}
+) => {
+	// Set CORS headers
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Request-Method', 'OPTIONS, GET, POST');
+	res.setHeader(
+		'Access-Control-Allow-Headers',
+		'Origin, X-Requested-With, Content-Type, Accept'
+	);
+
+	if (req.method === 'GET') {
+		// Check if the request is for one of the files in the images directory
+		if (req.url?.startsWith('/images/')) {
+			try {
+				const file = await fs.readFile(`./${req.url}`);
+				res.writeHead(200, {
+					'Content-Type': 'image/jpeg',
+				});
+				res.write(file);
+				res.end();
+			} catch (err) {
+				logger.error(err);
+				res.writeHead(404);
+				res.end();
+			}
+		}
+	} else if (req.method === 'POST') {
+		if (req.url === '/api/upload') {
+			const cookies = parseCookies(req);
+
+			if (!cookies['next-auth.session-token']) {
+				if (!cookies['__Secure-next-auth.session-token']) {
+					logger.error('Missing token');
+					res.writeHead(400, {
+						'Content-Type': 'text/plain',
+					});
+					res.end('Missing token');
+					return;
+				}
+			}
+
+			// Verify auth token
+			const session = await verifyAccessToken(
+				cookies['next-auth.session-token'] ||
+					cookies['__Secure-next-auth.session-token']
+			);
+
+			if (!session) {
+				logger.error('Invalid token');
+				res.writeHead(400, {
+					'Content-Type': 'text/plain',
+				});
+				res.end('Invalid token');
+				return;
+			}
+
+			if (session.expires < new Date()) {
+				logger.error('Token expired');
+				res.writeHead(400, {
+					'Content-Type': 'text/plain',
+				});
+				res.end('Token expired');
+				return;
+			}
+
+			const form = formidable({ multiples: true });
+
+			form.parse(req, async (err, fields, files) => {
+				if (err) {
+					logger.error(err);
+					res.writeHead(err.httpCode || 400, {
+						'Content-Type': 'text/plain',
+					});
+					res.end(String(err));
+					return;
+				}
+
+				if (!fields.path_id) {
+					logger.error('Missing path_id');
+					res.writeHead(400, {
+						'Content-Type': 'text/plain',
+					});
+					res.end('Missing path_id');
+					return;
+				}
+
+				const data = {
+					id: fields.path_id as string,
+					files: files.images as formidable.File[],
+				};
+
+				const response = await handleUpload(data);
+
+				if (!response.length || response.length !== data.files.length) {
+					logger.error('Error saving images');
+					res.writeHead(500, {
+						'Content-Type': 'text/plain',
+					});
+					res.end('Error saving images');
+					return;
+				}
+
+				res.writeHead(200, {
+					'Content-Type': 'application/json',
+				});
+
+				res.end(JSON.stringify(response));
+			});
+
+			return;
+		}
+	}
+};
+
+const handleUpload = async (data: {
+	id: string;
+	files: formidable.File[];
+}) => {
+	const image_urls: ImageResult[] = [];
+
+	try {
+		await fs.mkdir(`${IMAGE_DIRECTORY}/${data.id}`, {
+			recursive: true,
+		});
+	} catch (err) {
+		logger.error(`Error creating directory ${data.id}`);
+		logger.error(err);
+	}
+
+	for (const file of data.files) {
+		try {
+			const filename = file.originalFilename?.replace(
+				/[^a-z0-9.]/gi,
+				'_'
+			);
+
+			if (!filename) {
+				throw new Error('Invalid filename');
+			}
+
+			const url = `${IMAGE_DIRECTORY}/${data.id}/${filename}`;
+
+			const oldFile = await fs.readFile(file.filepath);
+
+			await fs.writeFile(url, oldFile);
+
+			image_urls.push({
+				image_name: filename,
+				image_url: url,
+			});
+
+			logger.debug(`Saved ${filename}`);
+		} catch (err) {
+			logger.error(`Error saving ${file.newFilename}`);
+			logger.error(err);
+
+			image_urls.push({
+				image_name: file.newFilename,
+			});
+		}
+	}
+
+	return image_urls;
+};
--- apps/backend/src/server.ts
+++ apps/backend/src/server.ts
@@ -1,11 +1,7 @@
 import http from 'http';
-import formidable from 'formidable';
-import { promises as fs } from 'fs';
 import { Server } from 'socket.io';
 import { logger } from './utils/logger';
-import { handleUpload } from './lib/save_images';
-import parseCookies from './utils/parseCookies';
-import { verifyAccessToken } from './db';
+import { handleRequest } from './lib/server_handlers';
 
 export default class NTPServer {
 	private static instance: NTPServer;
@@ -14,123 +10,8 @@ export default class NTPServer {
 	private eventMap: { [key: string]: () => Promise<void> };
 
 	private constructor() {
-		this.httpServer = http.createServer(async (req, res) => {
-			// Set CORS headers
-			res.setHeader('Access-Control-Allow-Origin', '*');
-			res.setHeader(
-				'Access-Control-Request-Method',
-				'OPTIONS, GET, POST'
-			);
-			res.setHeader(
-				'Access-Control-Allow-Headers',
-				'Origin, X-Requested-With, Content-Type, Accept'
-			);
-
-			if (req.method === 'GET') {
-				// Check if the request is for one of the files in the images directory
-				if (req.url?.startsWith('/images/')) {
-					try {
-						const file = await fs.readFile(`./${req.url}`);
-						res.writeHead(200, {
-							'Content-Type': 'image/jpeg',
-						});
-						res.write(file);
-						res.end();
-					} catch (err) {
-						logger.error(err);
-						res.writeHead(404);
-						res.end();
-					}
-				}
-			} else if (req.method === 'POST') {
-				if (req.url === '/api/upload') {
-					const cookies = parseCookies(req);
-
-					if (!cookies['next-auth.session-token']) {
-						if (!cookies['__Secure-next-auth.session-token']) {
-							logger.error('Missing token');
-							res.writeHead(400, {
-								'Content-Type': 'text/plain',
-							});
-							res.end('Missing token');
-							return;
-						}
-					}
-
-					// Verify auth token
-					const session = await verifyAccessToken(cookies['next-auth.session-token'] || cookies['__Secure-next-auth.session-token']);
-
-					if (!session) {
-						logger.error('Invalid token');
-						res.writeHead(400, {
-							'Content-Type': 'text/plain',
-						});
-						res.end('Invalid token');
-						return;
-					}
-
-					if (session.expires < new Date()) {
-						logger.error('Token expired');
-						res.writeHead(400, {
-							'Content-Type': 'text/plain',
-						});
-						res.end('Token expired');
-						return;
-					}
-
-					const form = formidable({ multiples: true });
-
-					form.parse(req, async (err, fields, files) => {
-						if (err) {
-							logger.error(err);
-							res.writeHead(err.httpCode || 400, {
-								'Content-Type': 'text/plain',
-							});
-							res.end(String(err));
-							return;
-						}
-
-						if (!fields.path_id) {
-							logger.error('Missing path_id');
-							res.writeHead(400, {
-								'Content-Type': 'text/plain',
-							});
-							res.end('Missing path_id');
-							return;
-						}
-
-						const data = {
-							id: fields.path_id as string,
-							files: files.images as formidable.File[],
-						};
-
-						const response = await handleUpload(data);
-
-						if (
-							!response.length ||
-							response.length !== data.files.length
-						) {
-							logger.error('Error saving images');
-							res.writeHead(500, {
-								'Content-Type': 'text/plain',
-							});
-							res.end('Error saving images');
-							return;
-						}
-
-						res.writeHead(200, {
-							'Content-Type': 'application/json',
-						});
-						
-						res.end(JSON.stringify(response));
-					});
-
-					return;
-				}
-			}
-		});
+		this.httpServer = http.createServer(handleRequest);
 		this.wss = new Server(this.httpServer, {
-			maxHttpBufferSize: 1e8,
 			cors: {
 				origin: '*',
 			},
