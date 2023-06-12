53ff2f18c1d495a2c9634ac87aa7d2e096341485
kmanka1 <77521313+kmanka1@users.noreply.github.com>
2023-06-12 12:19:06 -0400

    Fixed panorama number control

--- apps/frontend/src/components/view-360-details.tsx
+++ apps/frontend/src/components/view-360-details.tsx
@@ -1,4 +1,5 @@
 import format from 'date-fns/format';
+import { type FormEvent } from 'react';
 import {
 	Card,
 	CardContent,
@@ -6,10 +7,13 @@ import {
 	CardHeader,
 	CardTitle,
 } from './ui/card';
-import {Input} from './ui/input';
+import { Button } from '@/components/ui/button';
+import { Input } from './ui/input';
 import { cn } from '@/lib/utils';
 import { Image360, Path } from 'database';
 import { PropsWithChildren, type ReactNode } from 'react';
+import { type SubmitHandler, useForm } from 'react-hook-form';
+import { CornerDownLeft } from 'lucide-react';
 
 type View360DetailsProps = {
 	index: number;
@@ -36,6 +40,28 @@ function DetailsRow(
 }
 
 export function View360Details(props: View360DetailsProps) {
+	type Inputs = {
+		pano: string;
+	};
+
+	const {
+		register,
+		handleSubmit,
+		control,
+		reset,
+		formState: { errors },
+	} = useForm<Inputs>();
+
+	const onSubmit = (event: FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
+		void (async () => {
+			await handleSubmit(((data) => {
+				props.onIndexChange(Number(data.pano) - 1);
+				reset();
+			}) as SubmitHandler<Inputs>)(event);
+		})();
+	};
+
 	return (
 		<Card className={cn('lg:col-span-2 lg:row-span-2', props.className)}>
 			<CardHeader>
@@ -99,13 +125,13 @@ export function View360Details(props: View360DetailsProps) {
 							{props.imageType === 'after'
 								? `${props.sortedImages?.[props.index]?.altitude.toFixed(1)} m`
 								: props.sortedImages?.[props.index]?.before.altitude != null
-								? `${props.sortedImages?.[props.index]?.before.altitude.toFixed(
+									? `${props.sortedImages?.[props.index]?.before.altitude.toFixed(
 										1
-								  )} m`
-								: 'N/A'}
+									)} m`
+									: 'N/A'}
 						</p>
 					</DetailsRow>
-                    <DetailsRow label="Image size">
+					<DetailsRow label="Image size">
 						<p>
 							{props.imageType === 'after'
 								? `${props.sortedImages?.[props.index]?.image_size / BigInt(1024)} kB`
@@ -113,15 +139,24 @@ export function View360Details(props: View360DetailsProps) {
 						</p>
 					</DetailsRow>
 				</div>
-				<CardDescription>Panorama capture</CardDescription>
-				<div className='flex flex-row items-center py-1'>
-				<Input className='w-14 h-8 p-0.5 mr-2 text-center' type="number" value={props.index + 1} step={1} min={0} max={props.sortedImages.length + 1} onChange={(event) => {
-					props.onIndexChange(Number(event.currentTarget.value) - 1);
-				}} />
-				{`/ ${
-					props.sortedImages.length || props.index + 1
-				}`}
-				</div>
+				<form onSubmit={onSubmit}>
+					<CardDescription>Panorama capture</CardDescription>
+					<div className='flex flex-row items-center py-1'>
+						<Input
+							className='w-12 h-8 p-0.5 mr-2 text-center text-base'
+							type="string"
+							{...register('pano', { required: true, max: props.sortedImages.length, min: 1 })}
+							placeholder={String(props.index + 1)}
+						/>
+						{`/ ${props.sortedImages.length || props.index + 1
+							}`}
+						<span className="px-4">
+							<Button variant="secondary" type="submit" className="h-8 w-8 p-0.5" disabled={errors.pano !== undefined}>
+								<CornerDownLeft className="h-4" />
+							</Button>
+						</span>
+					</div>
+				</form>
 			</CardContent>
 		</Card>
 	);
