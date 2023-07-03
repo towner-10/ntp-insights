56713bdb670527f940d8e5fb420cfff418b47f00
Collin Town <collintown@gmail.com>
2023-07-02 21:04:12 -0400

    Storm hashtag

--- apps/frontend/src/server/api/routers/search.ts
+++ apps/frontend/src/server/api/routers/search.ts
@@ -253,6 +253,7 @@ export const searchRouter = createTRPCRouter({
 
 				if (location.city) response.push(location.city);
 				if (location.state) response.push(location.state);
+				if (location.stateCode) response.push(`#${location.stateCode}Storm`);
 
 				return response;
 			}
