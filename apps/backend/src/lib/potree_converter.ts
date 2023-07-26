89a015d26d6cc7a6c13588390182154162a0bb21
Collin Town <collintown@gmail.com>
2023-07-26 18:31:54 -0400

    Add potree conversion support to backend - Requires PotreeConverter to be added to project

---
+++ apps/backend/src/lib/potree_converter.ts
@@ -0,0 +1,34 @@
+import { spawn } from 'child_process';
+import { logger } from '../utils/logger';
+
+const isWindows = process.platform === 'win32';
+
+export const potreeConverter = async (input: string, output: string) => {
+	const potreeConverterPath = isWindows
+		? './potree/PotreeConverter.exe'
+		: './potree/PotreeConverter';
+
+	await new Promise((resolve, reject) => {
+		const potreeConverterProcess = spawn(potreeConverterPath, [
+			input,
+			'-o',
+			output,
+		]);
+
+		potreeConverterProcess.stderr.on('data', (data) => {
+            logger.error(`PotreeConverter error: ${data}`);
+			reject(data);
+		});
+
+        potreeConverterProcess.on('exit', (code) => {
+            if (code !== 0) {
+                logger.error(`PotreeConverter exited with code ${code}`);
+                return reject(code);
+            }
+
+
+            logger.success(`PotreeConverted ${input} successfully to ${output}`);
+            return resolve(code);
+        });
+	});
+};
